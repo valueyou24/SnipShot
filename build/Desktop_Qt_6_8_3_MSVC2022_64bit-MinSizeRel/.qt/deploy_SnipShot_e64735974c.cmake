c6b6138ca8657ecdb72ac520308c29da4d35133c
Your Name <you@example.com>
2026-06-02 11:21:39 +0800

    Add demo GIF to README and update .gitignore to exclude build directories

--- build/Desktop_Qt_6_8_3_MSVC2022_64bit-MinSizeRel/.qt/deploy_SnipShot_e64735974c.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/Git/QT_Projects/SnipShot/build/Desktop_Qt_6_8_3_MSVC2022_64bit-MinSizeRel/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/SnipShot-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE D:/Git/QT_Projects/SnipShot/build/Desktop_Qt_6_8_3_MSVC2022_64bit-MinSizeRel/SnipShot.exe
-    GENERATE_QT_CONF
-)
